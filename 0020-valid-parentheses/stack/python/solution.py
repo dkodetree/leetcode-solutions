01a94aafc3c7f1dc1ddfe8b9422ddebd8fb7f67a
DKodeTree <208700321+dkodetree@users.noreply.github.com>
2025-06-21 19:34:30 +0530

    Update solution.py
    TC = O(n), SC = O(n)

--- 0020-valid-parentheses/stack/python/solution.py
+++ 0020-valid-parentheses/stack/python/solution.py
@@ -1,14 +1,15 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        brackets_mapping = { ")" : "(", "]" : "[", "}" : "{" } # close -> open
+        bracket_mapping = { "]": "[", ")": "(", "}": "{"}  # close -> open
         stack = []
-    
-        for ch in s:
-            if ch in brackets_mapping.values():
-                stack.append(ch)
-            elif ch in brackets_mapping and stack and stack[-1] == brackets_mapping[ch]: # stack[-1] is top
-                stack.pop()
-            else:   
-                return False
 
+        for ch in s:
+            if ch in bracket_mapping.values():  # open
+                stack.append(ch)
+            else:   # close
+                if stack and stack[-1] == bracket_mapping[ch]:
+                    stack.pop()
+                else:
+                    return False
+        
         return not stack

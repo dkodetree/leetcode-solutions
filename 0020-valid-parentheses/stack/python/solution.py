b28b477f6f6829a1fd0153cf5cd9985f04103978
DKodeTree <208700321+dkodetree@users.noreply.github.com>
2025-06-21 19:19:47 +0530

    Update solution.py

--- 0020-valid-parentheses/stack/python/solution.py
+++ 0020-valid-parentheses/stack/python/solution.py
@@ -6,9 +6,9 @@ class Solution:
         for ch in s:
             if ch in brackets_mapping.values():
                 stack.append(ch)
-            elif ch in brackets_mapping and stack and brackets_mapping[ch] == stack[-1]: # stack[-1] is top
+            elif ch in brackets_mapping and stack and stack[-1] == brackets_mapping[ch]: # stack[-1] is top
                 stack.pop()
             else:   
                 return False
 
-        return True if not stack else False
+        return not stack

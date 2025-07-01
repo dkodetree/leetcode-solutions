40e791dd1db0afbc3c16eadeb7b0a87ab67fba42
DKodeTree <208700321+dkodetree@users.noreply.github.com>
2025-07-02 00:25:14 +0530

    Sorting solution for Contains Duplicate
    TC = O(n log n), SC = O(1) or O(n) depending on sorting's internal implementation

---
+++ 0217-contains-duplicate/sorting/python/solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        nums.sort()
+        for idx in range(1, len(nums)):
+            if nums[idx] == nums[idx - 1]:
+                return True
+        return False

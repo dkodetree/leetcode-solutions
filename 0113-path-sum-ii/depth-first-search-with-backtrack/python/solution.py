0c26c489a4c6939d336a857fe782d0ee3af9e1d0
DKodeTree <208700321+dkodetree@users.noreply.github.com>
2025-09-03 03:09:03 +0530

    DFS with backtracking solution for path sum ii

---
+++ 0113-path-sum-ii/depth-first-search-with-backtrack/python/solution.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
+        res = []
+        path = []
+
+        def find_paths(node, remaining_sum): #DFS with Backtracking
+            if not node:
+                return
+            path.append(node.val)
+            if not node.left and not node.right and node.val == remaining_sum: 
+                res.append(path.copy())
+            else:
+                find_paths(node.left, remaining_sum - node.val) 
+                find_paths(node.right, remaining_sum - node.val) 
+            path.pop() # backtrack
+
+        find_paths(root, targetSum)
+        return res

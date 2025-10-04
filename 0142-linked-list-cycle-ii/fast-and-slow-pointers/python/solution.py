a0111d1be321f73da9345649f7822abfb36eaffd
DKodeTree <208700321+dkodetree@users.noreply.github.com>
2025-10-04 12:29:18 +0530

    Linked List Cycle II - Fast and Slow Pointers Updated solution
    TC = O(n), SC = O(1)

--- 0142-linked-list-cycle-ii/fast-and-slow-pointers/python/solution.py
+++ 0142-linked-list-cycle-ii/fast-and-slow-pointers/python/solution.py
@@ -6,14 +6,19 @@
 
 class Solution:
     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # Step 1: Detect the Cycle
         slow, fast = head, head
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
             if slow == fast:
-                entry = head
-                while entry != fast:
-                    entry = entry.next
-                    fast = fast.next
-                return entry
-        return None
+                break   # cycle detected  
+        else:
+            return None
+
+        # Step 2: Find the start node of the Cycle
+        slow1 = head
+        while slow != slow1:
+            slow = slow.next
+            slow1 = slow1.next
+        return slow

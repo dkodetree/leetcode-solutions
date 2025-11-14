81cfcb47b943c37e4023d2ec9e1860a23c3c7f3e
DKodeTree <208700321+dkodetree@users.noreply.github.com>
2025-11-14 22:24:23 +0530

    Update solution.py

--- 0509-fibonacci-number/bottom-up-dynamic-programming/python/solution.py
+++ 0509-fibonacci-number/bottom-up-dynamic-programming/python/solution.py
@@ -1,11 +1,9 @@
 class Solution:
     def fib(self, n: int) -> int:
         dp = {}     # Dictionary for Tabulation
-        
         for i in range(n + 1):
             if i <= 1:
                 dp[i] = i
             else:
                 dp[i] = dp[i - 1] + dp[i - 2]
-    
         return dp[n]

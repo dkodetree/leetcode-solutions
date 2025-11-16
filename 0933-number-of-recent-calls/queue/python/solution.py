08a8064179f69815396a9142a01e6a7ef886ecd9
DKodeTree <208700321+dkodetree@users.noreply.github.com>
2025-11-16 13:58:11 +0530

    Update solution.py

--- 0933-number-of-recent-calls/queue/python/solution.py
+++ 0933-number-of-recent-calls/queue/python/solution.py
@@ -11,8 +11,6 @@ class RecentCounter:
         
         return len(self.recent_counter)
 
-
-
 # Your RecentCounter object will be instantiated and called as such:
 # obj = RecentCounter()
 # param_1 = obj.ping(t)

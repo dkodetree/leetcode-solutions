14886dcde36f792d368a622cb7d94dfe2e3a3fe9
DKodeTree <208700321+dkodetree@users.noreply.github.com>
2025-11-16 15:07:12 +0530

    Javascript solution for delete node in a linked list

---
+++ 0237-delete-node-in-a-linked-list/inplace-node-deletion/javascript/solution.js
@@ -0,0 +1,16 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} node
+ * @return {void} Do not return anything, modify node in-place instead.
+ */
+var deleteNode = function(node) {
+    node.val = node.next.val;        // Copy next node's value
+    node.next = node.next.next;      // Skip next node
+    // JavaScript garbage collector automatically frees memory of the skipped node
+};

5866d9008a081ce1d98712e26076dc8ac2f18f14
DKodeTree <208700321+dkodetree@users.noreply.github.com>
2026-01-22 02:49:06 +0530

    Revised variable names

--- 1122-relative-sort-array/custom-sort/python/solution.py
+++ 1122-relative-sort-array/custom-sort/python/solution.py
@@ -1,10 +1,10 @@
 class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
-        arr2_index_map = {ele: idx for idx, ele in enumerate(arr2)}
+        order = {ele: idx for idx, ele in enumerate(arr2)}
         
-        def key(num):
-            if num in arr2_index_map:
-                return (0, arr2_index_map[num])
+        def relative_key(num):
+            if num in order:
+                return (0, order[num])
             return (1, num)
         
-        return sorted(arr1, key = key)
+        return sorted(arr1, key=relative_key)

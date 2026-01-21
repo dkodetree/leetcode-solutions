78f882ddd983bca626996dcedc3547d9883c3116
DKodeTree <208700321+dkodetree@users.noreply.github.com>
2026-01-22 03:18:33 +0530

    Custom Sort with Inline Key (Lambda) in Python
    TC = O(nlogn +m), SC = O(n + m),  where n is the size of arr1, and m is the size of arr2

---
+++ 1122-relative-sort-array/custom-sort-with-inline-composite-key/python/solution.py
@@ -0,0 +1,4 @@
+class Solution:
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        order = {ele: idx for idx, ele in enumerate(arr2)}        
+        return sorted(arr1, key=lambda x: (order.get(x, float("inf")), x))

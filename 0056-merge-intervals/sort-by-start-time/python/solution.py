49c8686dd823cba7a2e6c3145499db6eda6060cb
DKodeTree <208700321+dkodetree@users.noreply.github.com>
2026-02-13 01:51:16 +0530

    Update solution.py

--- 0056-merge-intervals/sort-by-start-time/python/solution.py
+++ 0056-merge-intervals/sort-by-start-time/python/solution.py
@@ -5,7 +5,7 @@ class Solution:
 
         for start, end in intervals[1:]:
             prev_end = res[-1][1]
-            if prev_end >= start: # overlapping
+            if start <= prev_end: # overlapping
                 res[-1][1] = max(prev_end, end)
             else:
                 res.append([start, end])

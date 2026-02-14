ff0bd2cd631eee98388773d3c3be22645cc5f9e2
DKodeTree <208700321+dkodetree@users.noreply.github.com>
2026-02-15 00:44:23 +0530

    Update solution.py

--- 0731-my-calendar-ii/overlap-tracking/python/solution.py
+++ 0731-my-calendar-ii/overlap-tracking/python/solution.py
@@ -1,16 +1,16 @@
 class MyCalendarTwo:
 
     def __init__(self):
-        self.calendar = []
-        self.overlaps = [] # Stores intervals that are already double-booked
+        self.calendar = []  # Stores all booked events
+        self.overlaps = []  # Stores overlapping portins of booked events (double-booking portions)
 
     def book(self, event_start: int, event_end: int) -> bool:
-        # If new interval overlaps with the existing overlapped regions, then don't book
+        # If new event overlaps with the existing overlapping portions, then don't book
         for start, end in self.overlaps:
             if max(event_start, start) < min(event_end, end):
                 return False
         
-        # If there are any overlaps with the existing calendar, then add the overlapped region to the overlaps list
+        # If there are any overlaps with the existing booked events, then add the overlapping portions to the overlaps list
         for start, end in self.calendar:
             intersect_start, intersect_end = max(event_start, start), min(event_end, end)
             if intersect_start < intersect_end:

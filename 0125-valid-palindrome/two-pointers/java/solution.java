3195d0d9975fccc7616fba8351c9aa8e97a526f5
DKodeTree <208700321+dkodetree@users.noreply.github.com>
2026-03-31 02:19:52 +0530

    Java Two pointers solution for Valid palindrome

---
+++ 0125-valid-palindrome/two-pointers/java/solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            char charLeft = s.charAt(left);
+            char charRight = s.charAt(right);
+
+            if (!Character.isLetterOrDigit(charLeft)) {
+                left++;
+            } 
+            else if (!Character.isLetterOrDigit(charRight)) {
+                right--;
+            } 
+            else {
+                if (Character.toLowerCase(charLeft) != Character.toLowerCase(charRight)) {
+                    return false;
+                }
+                left++;
+                right--;
+            }
+        }
+        return true;
+    }
+}

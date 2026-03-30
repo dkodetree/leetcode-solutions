2ce969439c1f9a7afc26ee291316b5536b036b5e
DKodeTree <208700321+dkodetree@users.noreply.github.com>
2026-03-31 00:55:44 +0530

    Rename variables

--- 0125-valid-palindrome/reverse-comparison/python/solution.py
+++ 0125-valid-palindrome/reverse-comparison/python/solution.py
@@ -1,4 +1,4 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        valid_chars = [char.lower() for char in s if char.isalnum()]
-        return valid_chars == valid_chars[::-1]
+        filtered = [char.lower() for char in s if char.isalnum()]
+        return filtered == filtered[::-1]

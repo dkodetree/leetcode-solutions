986d9ecbbe8e6696c8fb9a9223fea2558337727c
DKodeTree <208700321+dkodetree@users.noreply.github.com>
2026-04-13 21:27:57 +0530

    Cpp solution for Implement Queue Using Stack
    TC : Push - O(1), Pop - Amortized O(1), Peek - Amortized O(1), Empty - O(1)

---
+++ 0232-implement-queue-using-stacks/two-stacks/cpp/solution.cpp
@@ -0,0 +1,46 @@
+class MyQueue {
+private:
+    stack<int> stack_in, stack_out;
+
+    // Helper to transfer elements only when the output stack is empty
+    void move_if_needed() {
+        if (stack_out.empty()) {
+            while (!stack_in.empty()) {
+                stack_out.push(stack_in.top());
+                stack_in.pop();
+            }
+        }
+    }
+    
+public:
+    MyQueue() {}
+    
+    void push(int x) {
+        stack_in.push(x);
+    }
+    
+    int pop() {
+        move_if_needed();
+        int val = stack_out.top();
+        stack_out.pop();
+        return val;
+    }
+    
+    int peek() {
+        move_if_needed();
+        return stack_out.top();
+    }
+    
+    bool empty() {
+        return stack_in.empty() && stack_out.empty();
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */

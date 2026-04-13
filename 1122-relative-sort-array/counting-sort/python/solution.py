ac4aa3e4ade6e2e4d0db9e96a7bee57a1a679779
DKodeTree <208700321+dkodetree@users.noreply.github.com>
2026-04-14 01:34:34 +0530

    Added comments

--- 1122-relative-sort-array/counting-sort/python/solution.py
+++ 1122-relative-sort-array/counting-sort/python/solution.py
@@ -2,14 +2,19 @@ class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
         max_num = max(arr1)
         count = [0] * (max_num + 1)
+
+        # Count frequency of elements in arr1
         for num in arr1:
             count[num] += 1
         
         res = []
+        # Relative Sorting using arr2
         for num in arr2:
             res.extend([num] * count[num])
             count[num] = 0
         
+        # Processing the remaining elements in ascending order
         for num in range(max_num + 1):
             res.extend([num] * count[num])
+
         return res

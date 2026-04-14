c8e1f8bb7c2c26c38557f7df821c62069a73e87c
DKodeTree <208700321+dkodetree@users.noreply.github.com>
2026-04-14 22:40:23 +0530

    Java solution for Linked List Cycle (Hashset Approach)
    TC = O(n), SC = O(n)

---
+++ 0141-linked-list-cycle/hashset/java/solution.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        Set<ListNode> visited = new HashSet<>();
+        ListNode cur = head;
+
+        while (cur != null) {
+            if (visited.contains(cur)) {
+                return true;
+            }
+            visited.add(cur);
+            cur = cur.next;
+        }
+        return false;
+    }
+}

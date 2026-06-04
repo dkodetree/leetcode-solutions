3dc2742bf3d0d41c7e23623920f13fa4bb999ae5
DKodeTree <208700321+dkodetree@users.noreply.github.com>
2026-06-05 02:29:21 +0530

    Cpp solution for wo Pointers Approach - Count Number of Fair Pairs
    TC = O(n log n)

---
+++ 2563-count-the-number-of-fair-pairs/two-pointers/cpp/solution.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    long long countFairPairs(vector<int>& nums, int lower, int upper) {
+        sort(nums.begin(), nums.end());
+        return countPairs(nums, upper) - countPairs(nums, lower - 1);
+    }
+
+private:
+    // Helper to count pairs with sum less than or equal to upperBound
+    long long countPairs(const vector<int>& nums, int upper_bound) {
+        long long count = 0;
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while (left < right) {
+            int pair_sum = nums[left] + nums[right];
+            if (pair_sum <= upper_bound) {
+                count += (right - left);
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return count;
+    }
+    
+};

63cb400fa5a9aa7b0c0bf4cf4cea5f0c40173e2f
DKodeTree <208700321+dkodetree@users.noreply.github.com>
2026-06-05 02:32:19 +0530

    Updated variable name and comments

--- 2563-count-the-number-of-fair-pairs/two-pointers/python/solution.py
+++ 2563-count-the-number-of-fair-pairs/two-pointers/python/solution.py
@@ -2,16 +2,18 @@ class Solution:
     def countFairPairs(self, nums: List[int], lower: int, upper: int) -> int:
         nums.sort()
 
-        def count_pairs(upper_bound) -> int: #count pairs with sum less than or equal to upper_bound
+        # Helper to count pairs with sum less than or equal to upper_bound
+        def count_pairs(upper_bound) -> int: 
+            count = 0
             left, right = 0, len(nums) - 1
-            cnt = 0
+
             while left < right:
                 pair_sum = nums[left] + nums[right]
                 if pair_sum <= upper_bound:
-                    cnt += right - left
+                    count += right - left
                     left += 1
                 else:
                     right -= 1
-            return cnt
+            return count
 
         return count_pairs(upper) - count_pairs(lower - 1)

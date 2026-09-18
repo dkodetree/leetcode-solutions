0e8fbc3274565756107fed64c96238b0af993440
DKodeTree <208700321+dkodetree@users.noreply.github.com>
2026-09-19 03:59:32 +0530

    Using vector instead of map
    TC = O(n), SC = O(1)

--- 1930-unique-length-3-palindromic-subsequences/frequency-tracking/cpp/solution.cpp
+++ 1930-unique-length-3-palindromic-subsequences/frequency-tracking/cpp/solution.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
     int countPalindromicSubsequence(string s) {
-        unordered_set<string> res;
+        unordered_set<int> res;
         unordered_set<char> left;
-        unordered_map<char, int> right;
+        vector<int> right(26, 0);
         for (char ele : s) {
-            right[ele]++;
+            right[ele - 'a']++;
         }
 
-        for (char mid : s) {    // atmost n
-            right[mid]--;
-            for (char outer : left) {   // atmost 26
-                if (right[outer] > 0) {
-                    res.insert(string(1, outer) + mid);
+        for (char mid : s) {    // at most n
+            int mid_idx = mid - 'a';
+            right[mid_idx]--; 
+
+            for (char outer : left) {   // at most 26
+                int outer_idx = outer - 'a';
+                if (right[outer_idx] > 0) {
+                    res.insert(outer_idx * 26 + mid_idx); // encoding (outer, mid) as an unique integer
                 }
             }
-            left.insert(mid);
+            left.insert(mid); 
         }
         return res.size();
     }

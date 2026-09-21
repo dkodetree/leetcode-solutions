4ccfcf3a0551059e6953e766913f6a0b278b0623
DKodeTree <208700321+dkodetree@users.noreply.github.com>
2026-09-21 18:19:50 +0530

    Recursive dfs for n-ary tree preorder traversal in cpp
    TC =O(n), SC =O(n)

---
+++ 0589-n-ary-tree-preorder-traversal/recursive-depth-first-search/cpp/solution.cpp
@@ -0,0 +1,38 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<int> preorder(Node* root) {
+        vector<int> result;
+        dfs(root, result);
+        return result;
+    }
+
+private:
+    void dfs(Node* node, vector<int>& result) {
+        if (node) {
+            result.push_back(node->val);
+            for (Node* child : node->children) {
+                dfs(child, result);
+            }
+        }
+    }
+};

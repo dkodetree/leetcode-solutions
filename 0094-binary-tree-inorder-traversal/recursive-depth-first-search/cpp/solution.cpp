60123ca35ca9e336e5cf33b3139089c5c132b602
DKodeTree <208700321+dkodetree@users.noreply.github.com>
2026-09-21 17:07:28 +0530

    Stepdown rule refinement in structure

--- 0094-binary-tree-inorder-traversal/recursive-depth-first-search/cpp/solution.cpp
+++ 0094-binary-tree-inorder-traversal/recursive-depth-first-search/cpp/solution.cpp
@@ -11,16 +11,17 @@
  */
 class Solution {
 public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> result;
+        inorder(root, result);
+        return result;
+    }
+
+private:
     void inorder(TreeNode* node, vector<int>& result) {
         if (!node) return;
         inorder(node->left, result);
         result.push_back(node->val);
         inorder(node->right, result);
-    }
-
-    vector<int> inorderTraversal(TreeNode* root) {
-        vector<int> result;
-        inorder(root, result);
-        return result;
     }
 };

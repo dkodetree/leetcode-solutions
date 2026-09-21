4672731c4a95bba9708310526cbedd04b883a5fe
DKodeTree <208700321+dkodetree@users.noreply.github.com>
2026-09-21 17:37:20 +0530

    Iterative DFS for preorder traversal in java
    TC = O(n), SC =O(n)

---
+++ 0144-binary-tree-preorder-traversal/iterative-depth-first-search/java/solution.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) return result;
+
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            TreeNode cur = stack.pop();
+            if (cur != null) {
+                result.add(cur.val);
+                if (cur.right != null)  stack.push(cur.right);
+                if (cur.left != null)   stack.push(cur.left);
+            }
+        }
+        return result;
+    }
+}

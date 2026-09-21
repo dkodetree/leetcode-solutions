93a2abc5c7404c427ee0fb6108246c3da98c8303
DKodeTree <208700321+dkodetree@users.noreply.github.com>
2026-09-21 15:39:22 +0530

    Iterative DFS for Postorder traversal in CPP
    TC =O(n), SC =O(n)

---
+++ 0145-binary-tree-postorder-traversal/iterative-depth-first-search/cpp/solution.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> result;
+        vector<TreeNode*> stack;
+        stack.push_back(root);
+
+        while (!stack.empty()) {
+            TreeNode* cur = stack.back();
+            stack.pop_back();
+            if (cur) {
+                result.push_back(cur->val);
+                stack.push_back(cur->left);
+                stack.push_back(cur->right);
+            }
+        }
+
+        reverse(result.begin(), result.end()); // reverse to convert root-right-left into left-right-root (ie.postorder)
+        return result;
+    }
+};

26406a27b74803e4a42a3583c292c402c9c95006
DKodeTree <208700321+dkodetree@users.noreply.github.com>
2026-09-21 13:56:45 +0530

    Iterative DFS for Inorder traversal in CPP
    TC =O(n), Sc =O(n)

---
+++ 0094-binary-tree-inorder-traversal/iterative-depth-first-search/cpp/solution.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> res;
+        stack<TreeNode*> stack;
+        TreeNode* cur = root;
+
+        while (cur || !stack.empty()) {
+            // Go as left as possible
+            while (cur) {
+                stack.push(cur);
+                cur = cur->left;
+            }
+
+            // Process the node
+            cur = stack.top();
+            stack.pop();
+            res.push_back(cur->val);
+
+            // Go to the right subtree
+            cur = cur->right;
+        }
+        return res;
+    }
+};
